5038d995084b6558a7b6bc06fbfa9e25f36aee55
traghave@in.ibm.com <traghave@in.ibm.com>
2021-11-03 09:04:50 +0530

    Initial commit

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-for i in {2031..2040}
+for i in {1501..2000}
 do
     echo "output: $i"
     folderName="argoapp-$i" 
-    mkdir instance2/$folderName
-    cp app.yaml instance2/$folderName/
+    mkdir instance4/$folderName
+    cp app.yaml instance4/$folderName/
 done
